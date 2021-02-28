5a83c65824edc94f5cffdae6b11626c2f93380f5
raisiec <75871695+raisiec@users.noreply.github.com>
2021-02-28 11:09:25 +0800

    Update Lesson12_Calendar.py

--- Lesson12_Calendar.py
+++ Lesson12_Calendar.py
@@ -12,13 +12,19 @@ def get_events():
       current_event = line.split(',')
       #print(current_event)
 
-      print(current_event[0], type(current_event[0]))
-      print(current_event[1], type(current_event[1]))
+      print(current_event[0])
+      print(current_event[1])
 
       event_date = datetime.strptime(current_event[1], '%d/%m/%y').date()
-      print(event_date, type(event_date))
+      print(event_date)
       current_event[1] = event_date
       list_events.append(current_event)
+  return list_events
+
+# To compare the difference between two given dates
+def between_days(date1, date2):
+  # To-Do
+  pass
 
 # Create a Canvas for display      
 root = Tk()
@@ -30,3 +36,12 @@ c.create_text(100,50,text='My countdown calendar')
 
 # get event list from a file
 events = get_events()
+# create a datetime object for comparison
+today = date.today()
+
+# looping the items in events list
+print('I am now in main loop!')
+for event in events:
+  event_name = event[0]
+  event_date = event[1]
+  print(event_name, event_date)
