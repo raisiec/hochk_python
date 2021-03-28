ca29e2eaf62e361fd3730c932c5ace5bafd850a0
raisiec <75871695+raisiec@users.noreply.github.com>
2021-03-28 11:09:54 +0800

    Update Lesson15_Sentence.py

--- Lesson15_Sentence.py
+++ Lesson15_Sentence.py
@@ -5,10 +5,11 @@
 
 import random
 import string
+from tkinter import Tk, Canvas
 
 # Banner
 print('Hour of Code Hong Kong')
-print('Welcome to Sentence Generator')
+print('Welcome to Sentence Generator\n')
 
 # Noun list
 noun_list = ['apple', 'dinosaur', 'ball', 'goat']
@@ -22,14 +23,25 @@ adverb_list = ['slowly', 'bravely', 'angrily', 'sadly']
 # Verb list
 verb_list = ['run', 'eat', 'fly', 'walk']
 
-# print out the list content
-#for i in range(len(noun_list)):
-#  print(noun_list[i])
+# Create a Canvas for display      
+root = Tk()
+c = Canvas(root, width=800, height=800, bg='red')
+c.pack()
 
-noun = random.choice(noun_list)
-adj = random.choice(adjective_list)
-adv = random.choice(adverb_list)
-verb = random.choice(verb_list)
+# Add text for the Canvas
+c.create_text(100,50,text='My Sentence Generator')
+vertical_position = 100
 
-my_sentence = noun + verb + adj + adv
-print(my_sentence)
+# Generate 20 randomized sentences
+for i in range(20):
+  noun = random.choice(noun_list)
+  adj = random.choice(adjective_list)
+  adv = random.choice(adverb_list)
+  verb = random.choice(verb_list)
+  my_sentence = 'My super sentence is: ' + noun + ' ' + verb + ' ' + adj + ' ' + adv
+  print(my_sentence)
+
+  c.create_text(150, vertical_position, text=my_sentence)
+  vertical_position = vertical_position + 30
+
+# end of program
