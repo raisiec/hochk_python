99dc609c5c0cc68ba4aceee11f3f0b59cc240d54
raisiec <75871695+raisiec@users.noreply.github.com>
2021-05-02 10:30:25 +0800

    Update Lesson18_Expert.py

--- Lesson18_Expert.py
+++ Lesson18_Expert.py
@@ -1,1 +1,17 @@
+# Hour of Code - Ask Expert
+# Author: Raisie
+# Date: May 2, 2021
 
+from tkinter import Tk, simpledialog, messagebox
+
+print('Ask The Expert - Capital City of The World')
+
+root = Tk()
+root.withdraw()
+
+while True:
+  query_country = simpledialog.askstring('Country', 'Type The Name of a Country:')
+
+  # How to print the user input data to your console?
+
+  
