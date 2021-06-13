7daf912fc62448e904c02e025d527157262203e3
raisiec <75871695+raisiec@users.noreply.github.com>
2021-06-13 11:03:53 +0800

    Update Lesson21_Matchmaker.py

--- Lesson21_Matchmaker.py
+++ Lesson21_Matchmaker.py
@@ -27,3 +27,22 @@ symbols = [u'\u2702', u'\u2702', u'\u2705', u'\u2705', u'\u2708', u'\u2708',
            u'\u2714', u'\u2714', u'\u2716', u'\u2716', u'\u2728', u'\u2728']
 
 print(symbols)
+
+# Randomize the predefined symbols
+random.shuffle(symbols)
+print(symbols)
+
+def show_symbol(x,y):
+  pass
+  # TBC
+
+# Create a 6 x 4 array buttons
+for x in range(6):
+  for y in range(4):
+    button = Button(command = lambda x=x, y=y: show_symbol(x,y), width=5, height=5)
+    button.grid(column=x, row=y)
+    buttons[x,y] = button
+    button_symbols[x,y] = symbols.pop()
+
+# Tk Mainloop here
+root.mainloop()
