f8d71626edf245c65316118d7157e6b7fee8b549
raisiec <75871695+raisiec@users.noreply.github.com>
2021-09-12 10:12:28 +0800

    Create Lesson23_Snake.py

---
+++ Lesson23_Snake.py
@@ -0,0 +1,71 @@
+# Coder: Raisie & daddy
+# Date: 12/9/2021
+# Project: Snake Game
+
+import turtle as t
+
+# Create the Snake object
+t.bgcolor('white')
+cater = t.Turtle()
+cater.shape('square')
+cater.color('red')
+cater.speed(2)
+cater.penup()
+
+while True:
+  cater.forward(1)
+
+# Create the Leaf object
+leaf = t.Turtle()
+leaf_shape = ((0,0),(14,2),(18,6),(20,20),(6,18),(2,14))
+t.register_shape('leaf', leaf_shape)
+leaf.shape('leaf')
+leaf.color('green')
+leaf.speed(0)
+
+leaf.setx(0)
+leaf.sety(0)
+leaf.showturtle()
+
+# Create a Text object
+text = t.Turtle()
+text.write('Press SPACE to start', align='center', font=('Arial', 16, 'bold'))
+text.hideturtle()
+
+# Create a score object
+
+score_turtle = t.Turtle()
+score_turtle.hideturtle()
+
+# This function will determine whether the snake is off the bounding window?
+def outside_window():
+  pass
+
+# This function will be executed when the game is over
+def game_over():
+  pass
+
+# This function will update the score turtle on the screen
+def display_score():
+  pass
+
+# This function will randomly place a place on the screen
+def place_leaf():
+  pass
+
+# The following 4 functions will control the movement of the snake
+def move_up():
+  pass
+
+def move_down():
+  pass
+
+def move_left():
+  pass
+
+def move_right():
+  pass
+
+# The following function will be executed when the game is ready to start
+def start_game():
+  pass
