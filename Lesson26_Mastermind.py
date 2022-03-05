6a05292f582a7eb1b177783e834d4a1970109e33
raisiec <75871695+raisiec@users.noreply.github.com>
2022-03-05 11:31:08 +0800

    Update Lesson26_Mastermind.py

--- Lesson26_Mastermind.py
+++ Lesson26_Mastermind.py
@@ -4,19 +4,30 @@
 
 import random
 
+# Variable used
 lives = 10
 heart_symbol = u'\u2764'
 colors = ['R', 'G', 'B', 'W', 'Y']
 
+# Ask player for the difficulty of this game
 length = int(input('\nWhat is the length of the secret color (2 / 3 / 4 / 5)? '))
-print(length)
 
+# Prepare the secret color list for guessing
 color_list = ''
 for i in range(length):
   color_list = color_list + random.choice(colors) 
-  print(color_list)
+  #print(color_list)
 
+# Main loop
 while (lives > 0):
-  print('\nWhat is the secret color (R, G, B, W, Y)? ')
-  guess = input()
-  print(guess)
+  print('lives left:' + heart_symbol * lives)
+  guess = input('\nWhat is the secret color (R, G, B, W, Y)? ')
+
+  if (color_list!= guess):
+    lives = lives - 1
+    print('You lost! Please try again!\n')
+  else:
+    print('You got the color correct!\n')
+    break
+
+# END
